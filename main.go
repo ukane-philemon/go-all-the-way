3f84e559ac8036527de4ad59934e3635cc4ac496
Dapo Michaels <dapomichaels@gmail.com>
2023-02-15 19:27:26 +0100

    boilerplate

--- main.go
+++ main.go
@@ -4,11 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
-
 	router.Run()
 }
